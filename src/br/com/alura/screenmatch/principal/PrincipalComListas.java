5e64f6095b6b6074f7aed48b1314778c5049b8d9
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-09-13 10:04:29 -0300

    Comandos de ordenação.

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -5,6 +5,8 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -26,6 +28,21 @@ public class PrincipalComListas {
                 System.out.println("Classificação " + filme.getClassificacao());
             }
         }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Lucas");
+
+        System.out.println(buscaPorArtista);
+        //pondo em ordem alfabética
+        Collections.sort(buscaPorArtista);
+        System.out.println("Agora segue em ordem alfabética: " + buscaPorArtista);
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando pelo ano: " + lista);
+
 
 
 
