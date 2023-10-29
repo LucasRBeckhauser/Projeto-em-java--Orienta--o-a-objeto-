bc5e0747c5f32c5f1b7ad43848783a65dcb4017c
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-10-29 11:25:42 -0300

    Vinculação da biblioteca Gson ao meu projeto, para converter uma string JSON em um objeto Java equivalente.

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,4 +1,27 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual filme você deseja ver: ");
+        var busca = scanner.nextLine();
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=febd95e4";
+        HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(endereco))
+            .build();
+    HttpResponse<String> response = client
+            .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+    }
 }
+
